1f04c1c47b5ec42a29b899c9cd733a84ca082f1f
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-23 12:07:06 +0530

    leads model created

--- leads/models.py
+++ leads/models.py
@@ -1,7 +1,17 @@
 from django.db import models
-from django.db.models.fields import CharField
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    pass
 
 
 class Lead(models.Model):
@@ -14,5 +24,5 @@ class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
-    phoned = models.BooleanField(default=False)
-    source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+    pass
