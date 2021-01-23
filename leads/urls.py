7878318c87d75307f430051b9ee3e647c7c7c845
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-23 17:41:24 +0530

    lead update and create

--- leads/urls.py
+++ leads/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import home_page, lead_detail, lead_create
+from .views import (home_page, lead_delete, lead_detail,
+                    lead_create, lead_update, lead_delete)
 
 app_name = "leads"
 urlpatterns = [
     path("", home_page),
     path('<int:pk>/', lead_detail),
-    path('create/', lead_create)
+    path('create/', lead_create),
+    path('<int:pk>/update/', lead_update),
+    path('<int:pk>/delete/', lead_delete)
 ]
--- leads/views.py
+++ leads/views.py
@@ -34,6 +34,28 @@ def lead_create(request):
     }
     return render(request, 'leads/create_lead.html', context)
 
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == 'POST':
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
+    context = {
+        "form": form,
+        "lead": lead
+    }
+    return render(request, 'leads/lead_update.html', context)
+
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads")
+
+
 # def lead_create(request):
 #     form = LeadModelForm()
 #     if request.method == "POST":
@@ -56,3 +78,27 @@ def lead_create(request):
 #         "form": form
 #     }
 #     return render(request, 'leads/create_lead.html', context)
+
+
+# def lead_update(request, pk):
+#     lead = Lead.objects.get(id=pk)
+#     form = LeadForm()
+#     if request.method == "POST":
+#         form = LeadForm(request.POST)
+#         if form.is_valid():
+#             first_name = form.cleaned_data['first_name']
+#             last_name = form.cleaned_data['last_name']
+#             age = form.cleaned_data['age']
+
+#             agent = Agent.objects.first()
+#             lead.first_name = first_name
+#             lead.last_name = last_name
+#             lead.age = age
+#             lead.save()
+
+#             return redirect("/leads")
+#     context = {
+#         "form": form,
+#         'lead': lead
+#     }
+#     return render(request, 'leads/lead_update.html', context)
