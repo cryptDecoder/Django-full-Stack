25080b7a6962dadce59f974c6ea1c84a8eb808dc
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-23 17:01:31 +0530

    lead form added

--- leads/urls.py
+++ leads/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home_page, lead_detail
+from .views import home_page, lead_detail, lead_create
 
 app_name = "leads"
 urlpatterns = [
     path("", home_page),
-    path('<pk>', lead_detail)
+    path('<int:pk>/', lead_detail),
+    path('create/', lead_create)
 ]
