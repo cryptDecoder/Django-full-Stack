01812fcba017f639ae4f355346d9cfeece24f859
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-23 13:04:31 +0530

    Leads model updated

--- leads/models.py
+++ leads/models.py
@@ -11,6 +11,9 @@ class User(AbstractUser):
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.user.username
+
     pass
 
 
@@ -25,4 +28,7 @@ class Lead(models.Model):
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
     agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
     pass
