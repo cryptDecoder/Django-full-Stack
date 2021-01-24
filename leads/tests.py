632b78466dde5795b92e8c01deb795ffc65979ff
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-24 09:01:35 +0530

    test cases begin

--- leads/tests.py
+++ leads/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
-
+from django.shortcuts import reverse
 # Create your tests here.
+
+
+class HomePageTest(TestCase):
+    def test_status_code(self):
+        response = self.client.get(reverse('home'))
+        print(response.status_code)
